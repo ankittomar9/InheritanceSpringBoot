ea5a7a277bf9f45a4a255f13e049a768a7447a70
Ankit <tomarankit78@gmail.com>
2025-03-05 18:57:20 +0530

    Inheritance ORM Learning Example Tables created Joined in joined SuperClass class

--- src/main/java/com/company/inheritancespringboot/mappedSuperClass/TablePerClass/Instructor.java
+++ src/main/java/com/company/inheritancespringboot/mappedSuperClass/TablePerClass/Instructor.java
@@ -2,7 +2,7 @@ package com.company.inheritancespringboot.mappedSuperClass.TablePerClass;
 
 import jakarta.persistence.Entity;
 
-@Entity(name="tpc_instructor")
+@Entity(name="msc_instructor")
 public class Instructor extends User {
     String company;
 }
